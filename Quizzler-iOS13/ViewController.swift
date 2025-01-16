f4587646b079ad70f3c3cefc8d32ad60a7e70057
victorgervac <victorgervacio33@gmail.com>
2025-01-16 12:39:18 -0700

    add timer

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -54,10 +54,10 @@ class ViewController: UIViewController {
         }else{
             questionNumber = 0
         }
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
-    func updateUI(){
+    @objc func updateUI(){
         questionLabel.text = quiz[questionNumber].text
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
