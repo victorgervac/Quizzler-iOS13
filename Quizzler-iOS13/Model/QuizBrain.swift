0bfe97b19b6b251914d8d014b5154e6e233566de
victorgervac <victorgervacio33@gmail.com>
2025-01-21 20:08:53 -0700

    mutating funcs

--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -7,7 +7,8 @@
 //
 
 import Foundation
-struct quizBrain {
+struct QuizBrain {
+   
     
     let quiz = [
               Question(q: "A slug's blood is green.", a: "True"),
@@ -25,12 +26,29 @@ struct quizBrain {
 
     ]
     var questionNumber = 0
-    func checkAnswer(_ userAnswer: String){
-        if userAnswer == Quiz[questionNumber].answer {
-            //            true
+    
+    
+    func checkAnswer(_ userAnswer: String)-> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            return true
         }else{
-            //            false
+            return false
         }
-        
+    }
+    func getQuestionText()-> String{
+        return quiz[questionNumber].text
+    }
+    
+    func getProgress() -> Float{
+        let progress = Float(questionNumber) / Float(quiz.count)
+        return progress
+    }
+    
+    mutating func nextQuestion(){
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        }else{
+            questionNumber = 0
+        }
     }
 }
